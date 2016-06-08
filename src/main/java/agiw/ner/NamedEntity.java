f67b11cf7b83b01b287d3008a4e0e1a65d209b9d
andybergon <andybergon@gmail.com>
2016-06-09 01:17:51 +0200

    get named entities with alchemyAPI

---
+++ src/main/java/agiw/ner/NamedEntity.java
@@ -0,0 +1,29 @@
+package agiw.ner;
+
+public class NamedEntity {
+	private String name;
+	private String type;
+
+	public NamedEntity(String name, String type) {
+		super();
+		this.name = name;
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
--- src/main/java/agiw/ner/alchemyapi/AlchemyAPITest.java
+++ src/main/java/agiw/ner/alchemyapi/AlchemyAPITest.java
@@ -1,42 +1,111 @@
 package agiw.ner.alchemyapi;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.lang.builder.ToStringStyle;
 
-import com.likethecolor.alchemy.api.*;
 import com.likethecolor.alchemy.api.Client;
 import com.likethecolor.alchemy.api.call.AbstractCall;
-import com.likethecolor.alchemy.api.call.AuthorCall;
-import com.likethecolor.alchemy.api.call.LanguageCall;
+import com.likethecolor.alchemy.api.call.RankedNamedEntitiesCall;
 import com.likethecolor.alchemy.api.call.type.CallTypeUrl;
-import com.likethecolor.alchemy.api.entity.AlchemyEntity;
-import com.likethecolor.alchemy.api.entity.LanguageAlchemyEntity;
+import com.likethecolor.alchemy.api.entity.NamedEntityAlchemyEntity;
 import com.likethecolor.alchemy.api.entity.Response;
 
-public class AlchemyAPITest {
-	public static void main(String[] args) {
-		final String apiKey = "d2f20d1fb2b74be0a0ab12a75dfe9b731981eaeb";
-		final Client client = new Client();
+import agiw.ner.NamedEntity;
 
+public class AlchemyAPITest {
+	private Client client;
+
+	public AlchemyAPITest() {
+		this.client = new Client();
+	}
+
+	public static void main(String[] args) {
+		String url = "http://www.lastampa.it/2015/03/20/cronaca/anna-abbagnale-fuori-pericolo-tornano-i-trenta-torinesi-dalla-tunisia-NldddrrapGzIdLUz7N2ABL/pagina.html";
+
+		AlchemyAPITest aat = new AlchemyAPITest();
+		aat.initialize();
+		aat.printEntitiesFromUrl(url);
+		aat.getEntitiesFromUrl(url);
+
+	}
+
+	public void initialize() {
+		String apiKey = "d2f20d1fb2b74be0a0ab12a75dfe9b731981eaeb";
 		client.setAPIKey(apiKey);
-		
-		
-		String urlString = "http://www.cnn.com/2009/CRIME/01/13/missing.pilot/index.html";
-	    final AbstractCall<LanguageAlchemyEntity> languageCall = new LanguageCall(new CallTypeUrl(urlString));
-	    
-	    try {
-	    	Response<LanguageAlchemyEntity> response = client.call(languageCall);
-	    	String responseString = response.toString(ToStringStyle.MULTI_LINE_STYLE);
+	}
+
+	public void printEntitiesFromUrl(String url) {
+		final AbstractCall<NamedEntityAlchemyEntity> entityCall = new RankedNamedEntitiesCall(new CallTypeUrl(url));
+
+		try {
+			Response<NamedEntityAlchemyEntity> response = this.client.call(entityCall);
+
+			System.out.println("Language: " + response.getLanguage());
+			System.out.println("Status: " + response.getStatus());
+			System.out.println("Status Info: " + response.getStatusInfo());
+			System.out.println("Text: " + response.getText());
+			System.out.println("Usage: " + response.getUsage());
+			System.out.println("URL: " + response.getURL());
+
+			String responseString = response.toString(ToStringStyle.MULTI_LINE_STYLE);
 			System.out.println(responseString);
+
+			NamedEntityAlchemyEntity entity;
+			final Iterator<NamedEntityAlchemyEntity> iter = response.iterator();
+
+			while (iter.hasNext()) {
+				entity = iter.next();
+				System.out.println("Text: " + entity.getText());
+				System.out.println("Type: " + entity.getType());
+				System.out.println("Score: " + entity.getScore());
+
+				System.out.println(entity.toString(ToStringStyle.MULTI_LINE_STYLE));
+			}
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-//			    final AbstractCall<AuthorAlchemyEntity> authorCall = new AuthorCall(new CallTypeUrl("http://www.politico.com/blogs/media/2012/02/detroit-news-ed-upset-over-romney-edit-115247.html"));
-//			    final Response response = client.call(authorCall);
-		//	    System.out.println(response);
-		//	    System.out.println();
 	}
+
+	public List<NamedEntity> getEntitiesFromUrl(String url) {
+		final AbstractCall<NamedEntityAlchemyEntity> entityCall = new RankedNamedEntitiesCall(new CallTypeUrl(url));
+		List<NamedEntity> entities = new ArrayList<NamedEntity>();
+
+		try {
+			Response<NamedEntityAlchemyEntity> response;
+			NamedEntityAlchemyEntity alchemyEntity;
+			NamedEntity namedEntity;
+			String entityName;
+			String entityType;
+
+			response = this.client.call(entityCall);
+
+			final Iterator<NamedEntityAlchemyEntity> iter = response.iterator();
+
+			while (iter.hasNext()) {
+				alchemyEntity = iter.next();
+
+				System.out.println("Text: " + alchemyEntity.getText());
+				System.out.println("Type: " + alchemyEntity.getType());
+				System.out.println("Score: " + alchemyEntity.getScore());
+
+				entityName = alchemyEntity.getText();
+				entityType = alchemyEntity.getType();
+				namedEntity = new NamedEntity(entityName, entityType);
+				entities.add(namedEntity);
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return entities;
+	}
+
 }
