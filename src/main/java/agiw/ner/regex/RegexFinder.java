936bf0861f9e0ef4986ac287008506ccac0e0f8b
luca <luca@luca-SATELLITE-L50-A-12W>
2016-06-09 17:05:23 +0200

    ( in telefono

--- src/main/java/agiw/ner/regex/RegexFinder.java
+++ src/main/java/agiw/ner/regex/RegexFinder.java
@@ -14,14 +14,15 @@ public class RegexFinder {
 	
 	public static void main(String[] args) {
 		String emailTest = "my email is andyb@libero.it, ajaj \n my other is adjaj@gmail.com. taas SdSDad@laskDa.org";
-		String phoneTest = "l mio numero è: +39 433902024, il numero di luca è 0693 8247289. Quello di simo: +3935422985. chiara è 0689939983.";
+		String phoneTest = "l mio numero è:  (+39) 433902024, il numero di luca è 0693 8247289. Quello di simo: +3935422985. chiara è 0689939983.";
 		String addressTest = "mia Via Eschiclo, 21. luca: viale amster casav 3/33/333, chiara è largo adjj ee 22/b e bla.";
 		String jobTest = "Ing. Paolo Merialdo, ing. paolo merialdo, dott.sa";
-
+		String test = emailTest+phoneTest+addressTest+jobTest;
+		
 		String emailRegex = "[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})";
 		String er0 = "[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}";
 
-		String phoneRegex = "([\\+(]?(\\d){2,}[)]?[- /\\.]?(\\d){2,}[- /\\.]?(\\d){2,}[- /\\.]?(\\d){2,}[- /\\.]?(\\d){2,})|([\\+(]?(\\d){2,}[)]?[- /\\.]?(\\d){2,}[- /\\.]?(\\d){2,}[- /\\.]?(\\d){2,})|([\\+(]?(\\d){2,}[)]?[- /\\.]?(\\d){2,}[- /\\.]?(\\d){2,})";
+		String phoneRegex = "([(]?[\\+(]?(\\d){2,}[)]?[- /\\.]?(\\d){2,}[- /\\.]?(\\d){2,}[- /\\.]?(\\d){2,}[- /\\.]?(\\d){2,})|([\\+(]?(\\d){2,}[)]?[- /\\.]?(\\d){2,}[- /\\.]?(\\d){2,}[- /\\.]?(\\d){2,})|([\\+(]?(\\d){2,}[)]?[- /\\.]?(\\d){2,}[- /\\.]?(\\d){2,})";
 		String pr0 = ".*[^0-9-( )] ([0-9-( )]+)$";
 		String pr1 = "(?:(?:\\+?1\\s*(?:[.-]\\s*)?)?(?:(\\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]‌​)\\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\\s*(?:[.-]\\s*)?)([2-9]1[02-9]‌​|[2-9][02-9]1|[2-9][02-9]{2})\\s*(?:[.-]\\s*)?([0-9]{4})\\s*(?:\\s*(?:#|x\\.?|ext\\.?|extension)\\s*(\\d+)\\s*)?";
 
@@ -34,7 +35,7 @@ public class RegexFinder {
 		String jobRegex = "(?i)(" + jobStart + ")"; // to finish
 
 		RegexFinder rf = new RegexFinder();
-		List<String> allMatches = rf.findRegexInString(emailRegex, emailTest);
+		List<String> allMatches = rf.findRegexInString(phoneRegex, test);
 
 		System.out.println(allMatches.toString());
 
