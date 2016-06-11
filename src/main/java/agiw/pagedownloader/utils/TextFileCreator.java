7aebe2929756ca39bf900b6de8057a665b5e084c
andybergon <andybergon@gmail.com>
2016-06-11 19:39:44 +0200

    fix 403, set user agent

--- src/main/java/agiw/pagedownloader/utils/TextFileCreator.java
+++ src/main/java/agiw/pagedownloader/utils/TextFileCreator.java
@@ -16,9 +16,10 @@ import org.jsoup.nodes.Document;
 import utils.PropertyFactor;
 
 public class TextFileCreator {
+	public static final String MOCK_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36";
 
 	public static void createFile(String lastname, String name, String url) {
-		Document doc;
+		Document retrievedDocument;
 		String urlEncoded, outputFolderPath, outputFilePath;
 		BufferedWriter htmlWriter;
 
@@ -27,30 +28,33 @@ public class TextFileCreator {
 		try {
 			outputFolderPath = pf.getStoragePath();
 			File outputFolder = new File(outputFolderPath);
-			
+
 			if (!outputFolder.exists()) {
 				outputFolder.mkdirs();
 			}
-			
+
 			urlEncoded = URLEncoder.encode(url, "UTF-8");
 			outputFilePath = outputFolderPath + lastname + "_" + name + "_" + urlEncoded + ".txt";
 
 			System.out.println("Requesting: " + url);
-			doc = Jsoup.connect(url).ignoreContentType(true).get();
+			retrievedDocument = Jsoup	.connect(url)
+										.ignoreContentType(true)
+										.userAgent(MOCK_AGENT)
+										.get();
 
 			FileOutputStream fos = new FileOutputStream(outputFilePath);
 			OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
 			htmlWriter = new BufferedWriter(osw);
-			htmlWriter.write(doc.toString());
+			htmlWriter.write(retrievedDocument.toString());
 			htmlWriter.close();
 
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (SocketTimeoutException e) {
-			System.err.println("SocketTimeoutException - Jsoup Error (?): 403 or other");
+			System.out.println("SocketTimeoutException - Jsoup Error (?): 403 or other");
 			//	e.printStackTrace();
 		} catch (HttpStatusException e) {
-			System.err.println("HttpStatusException - Jsoup Error (?): 403 or other");
+			System.out.println("HttpStatusException - Jsoup Error (?): 403 or other");
 			//	e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
